696666729a28ec87206e633915b97fb086108187
SumitRaaho <sumitkumar@raaho.in>
2024-07-17 00:15:52 +0530

    test version changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.alpharays.medprofilefma"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
